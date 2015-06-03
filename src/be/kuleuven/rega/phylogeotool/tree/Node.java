4edb610e717a5636d7015862fed82c6b170cc1d4
Ewout Vanden Eynden <ewout.vandeneynden@rega.kuleuven.be>
2015-06-03 17:33:46 +0200

    - Renamed KMedoidsToPhylo to ClusterAlgos - Moved the Tree, Node and Shape classes to a different directory

--- src/be/kuleuven/rega/phylogeotool/tree/Node.java
+++ src/be/kuleuven/rega/phylogeotool/tree/Node.java
@@ -1,11 +1,8 @@
-package be.kuleuven.rega.phylogeotool.tree;
+package be.kuleuven.rega.treedraw;
 
+import java.awt.Color;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-
-import be.kuleuven.rega.comparator.PhyloClusterSizeComparator;
-import be.kuleuven.rega.phylogeotool.tree.tools.NodeIndexProvider;
 
 public class Node {
 	private String data;
@@ -13,77 +10,135 @@ public class Node {
 	private Node parent;
 	private List<Node> children;
 	private List<Node> leafs;
-	private NodeIndexProvider nodeIndexProvider;
+	private double x = 0.0;
+	private double y = 0.0;
+	private double theta = 0.0;
+	private int size = 0;
+	private Color nodeColor;
 	
 	public Node() {
 		this.children = new ArrayList<Node>();
-	}
-	
-	public Node(NodeIndexProvider nodeIndexProvider) {
-		this.children = new ArrayList<Node>();
 		this.label = "";
-		this.nodeIndexProvider = nodeIndexProvider;
 	}
-	
-	public Node(String label, NodeIndexProvider nodeIndexProvider) {
+
+	public Node(String label) {
 		this.children = new ArrayList<Node>();
 		this.label = label;
-		this.nodeIndexProvider = nodeIndexProvider;
 	}
-	
+
 	public String getData() {
 		return data;
 	}
+
 	public void setData(String data) {
 		this.data = data;
 	}
+
 	public Node getParent() {
 		return parent;
 	}
+
 	public void setParent(Node parent) {
 		this.parent = parent;
 	}
+
 	public List<Node> getChildren() {
 		return children;
 	}
-	// Note that the biggest element has to be in the beginning and the lowest one in the end
-	public List<Node> getChildrenBiggestFirst() {
-		Collections.sort(this.getChildren(), new PhyloClusterSizeComparator());
-		return this.getChildren();
-	}	
+
+	// Note that the biggest element has to be in the beginning and the lowest
+	// one in the end
+	// public List<Node> getChildrenBiggestFirst() {
+	// Collections.sort(this.getChildren(), new PhyloClusterSizeComparator());
+	// return this.getChildren();
+	// }
 	public void addChild(Node node) {
 		this.children.add(node);
 	}
+
 	public boolean hasChildren() {
 		return children.size() != 0;
 	}
+
 	public String getLabel() {
 		return label;
 	}
+
 	public void setLabel(String label) {
 		this.label = label;
 	}
-	public int getIndex() {
-		return nodeIndexProvider.getIndex(label);
+
+	public double getX() {
+		return x;
 	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+	
+	public double getTheta() {
+		return theta;
+	}
+
+	public void setTheta(double theta) {
+		this.theta = theta;
+	}
+
 	public List<Node> getLeaves() {
-		if(leafs == null) {
+		if (leafs == null) {
 			this.leafs = new ArrayList<Node>();
-		} else if(leafs.size() > 0) {
+		} else if (leafs.size() > 0) {
 			return leafs;
 		}
 		visitNode(this);
 		return leafs;
 	}
+
 	public void visitNode(Node node) {
-	    if(node.hasChildren() && node.getChildren().get(0) != null) {
-	        visitNode(node.getChildren().get(0));
-	    }
-	    if(node.hasChildren() && node.getChildren().get(1) != null) {
-	        visitNode(node.getChildren().get(1));
-	    }
-	    if(!node.hasChildren()) {
-	        leafs.add(node);
-	    }
+		if (node.hasChildren() && node.getChildren().get(0) != null) {
+			visitNode(node.getChildren().get(0));
+		}
+		if (node.hasChildren() && node.getChildren().get(1) != null) {
+			visitNode(node.getChildren().get(1));
+		}
+		if (!node.hasChildren()) {
+			leafs.add(node);
+		}
+	}
+	
+	public int getSize() {
+		return this.size;
+	}
+	
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public void setColor(Color color) {
+		this.nodeColor = color;
+	}
+	
+	public Color getColor() {
+		return nodeColor;
+	}
+	
+	public static double polarToEucledianX(Node node) {
+		return node.getX()*Math.cos(node.getTheta());
+	}
+
+	public static double polarToEucledianY(Node node) {
+		return node.getX()*Math.sin(node.getTheta());
+	}
+	
+	public boolean hasParent() {
+		return getParent() != null;
 	}
 }
