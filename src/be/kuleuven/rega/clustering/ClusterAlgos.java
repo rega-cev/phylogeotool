03f1600e79367b9c67323fcce3d02aaab842ab1c
Ewout Vanden Eynden <ewout.vandeneynden@rega.kuleuven.be>
2015-11-25 17:02:01 +0100

    Fixed a bug

--- src/be/kuleuven/rega/clustering/ClusterAlgos.java
+++ src/be/kuleuven/rega/clustering/ClusterAlgos.java
@@ -26,67 +26,17 @@ public class ClusterAlgos {
 	}
 
 	public Tree getCluster(Tree tree, Node startNode, int nrClusters, int minimumClusterSize) {
-		
-		//kMedoids = new KMedoids(this.getNrOfClusters(), 100, allData);
-		//Map<ICluster, List<ICluster>> associationMap = kMedoids.calculate(true);
 		if(startNode == null) {
-//			return determineBestClusteringPieterIdea(tree.getRootNode(), nrClusters, tree);
 			return determineBestClusteringGuyIdea(tree.getRootNode(), nrClusters, tree, minimumClusterSize);
 		} else {
-//			return determineBestClusteringPieterIdea(startNode, nrClusters, tree);
 			return determineBestClusteringGuyIdea(startNode, nrClusters, tree, minimumClusterSize);
 		}
 	}
-
-	// TODO: Rework determination of best clusters
-	private Tree determineBestClustering(Node startNode, int nrClusters, Tree tree, int minimumClusterSize) {
+	
+	private Tree determineBestClusteringGuyIdea(Node startNode, int nrClusters, Tree tree, int minimumClusterSize) {
 		Tree toReturn = null;
 		midRootCluster = new MidRootCluster(nrClusters);
-//		for(int i = 1; i < nrClusters; i++) {
-//			midRootCluster.setNumberOfIClusters(i);
 		toReturn = midRootCluster.calculate(tree, startNode, new ClusterSizeComparator(), minimumClusterSize);
-			// TODO: Check if this part of the code is still necessary
-//			if(i >= 7 && midRootCluster.getNrBadClusters() >= ((1/6d)*i)) {
-//				midRootCluster.setNumberOfIClusters(i-1);
-//				toReturn = midRootCluster.calculate(startNode);
-//				return toReturn;
-//			}
-//		}
 		return toReturn;
 	}
-	
-	// TODO: Rework determination of best clusters
-		private Tree determineBestClusteringPieterIdea(Node startNode, int nrClusters, Tree tree, int minimumClusterSize) {
-			Tree toReturn = null;
-//			LinkedList<V> sortedClusters = new LinkedList<V>();
-			midRootCluster = new MidRootCluster(nrClusters);
-//			for(int i = 1; i < nrClusters; i++) {
-//				midRootCluster.setNumberOfIClusters(i);
-			toReturn = midRootCluster.calculate(tree, startNode, new ClusterSizeComparator(), minimumClusterSize);
-				//TODO: Figure out why it is slow and if we still need this
-//				// Sort the clusters to get the biggest ones in front
-//				for(V vertix:toReturn.getVertices()) {
-//					sortedClusters.add(vertix);
-//				}
-//				Collections.sort(sortedClusters, new JungClusterSizeComparator<V>(rootedTree));
-//				
-//				int numberOfSeqInClusters = 0;
-//				for(int j = 0; j < Math.floor(sortedClusters.size() * 0.50);j++) {
-//					numberOfSeqInClusters += JeblTools.getLeaves(rootedTree, ((jebl.evolution.graphs.Node)sortedClusters.get(j))).size();
-//				}
-//				
-//				if(numberOfSeqInClusters < Math.ceil(midRootCluster.getTotalAmountOfSequences() * 0.50)) {
-//					return toReturn;
-//				}
-//				
-//			}
-			return toReturn;
-		}
-		
-		private Tree determineBestClusteringGuyIdea(Node startNode, int nrClusters, Tree tree, int minimumClusterSize) {
-			Tree toReturn = null;
-			midRootCluster = new MidRootCluster(nrClusters);
-			toReturn = midRootCluster.calculate(tree, startNode, new ClusterSizeComparator(), minimumClusterSize);
-			return toReturn;
-		}
 }
